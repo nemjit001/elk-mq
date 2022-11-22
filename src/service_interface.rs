f6ab9b1d63c6f9f68dc93a3fa13ab8cb02d6de8c
Tijmen Verhoef <tijmenmenno@gmail.com>
2022-11-22 15:06:30 +0100

    fixed enqueue_dequeue

--- src/service_interface.rs
+++ src/service_interface.rs
@@ -1,45 +1,188 @@
-pub mod service_event;
+mod service_event;
+
+use std::collections::HashMap;
+use regex::Regex;
+use lazy_static::lazy_static;
+use redis::Commands;
+
+pub use service_event::ServiceEvent;
 
 #[derive(Debug, PartialEq)]
 pub enum InterfaceError {
+    ConnectionError(String),
+    JSONDumpError(String),
+    JSONParseError(String),
+    EnqueueError(String),
+    DequeueError(String),
     EmptyQueue,
     TimeoutExpired
 }
 
-pub type InterfaceResult<'a, T> = Result<T, InterfaceError>;
+pub type InterfaceResult<T> = Result<T, InterfaceError>;
 
-pub struct ServiceInterface<'a> {
-    queue_name: &'a str
+#[derive(Debug, PartialEq)]
+pub struct TimestampedEvent(u64, ServiceEvent);
+
+impl TimestampedEvent {
+    pub fn get_timestamp(&self) -> u64 {
+        self.0
+    }
+
+    pub fn get_event(&self) -> &ServiceEvent {
+        &self.1
+    }
 }
 
-impl<'a> ServiceInterface<'a> {
-    pub fn new(queue_name: &'a str) -> Self {
+pub struct ServiceInterface {
+    redis_client: redis::Client,
+    queue_name: String,
+    stream_name: String
+}
+
+impl ServiceInterface {
+    pub fn new(queue_name: &str, connection_url: &str) -> Self {
+        let redis_client = redis::Client::open(connection_url).unwrap();
+        let stream_name = std::fmt::format(format_args!("stream{}", queue_name));
+
         ServiceInterface {
-            queue_name
+            redis_client,
+            queue_name: String::from(queue_name),
+            stream_name
         }
     }
 
-    pub fn enqueue(&mut self, event: &service_event::ServiceEvent) -> InterfaceResult<()> {
+    fn extract_timestamp_from_event_key(key: &str) -> u64 {
+        lazy_static! {
+            static ref KEY_REGEX: Regex = Regex::new(r"(?P<timestamp>\d+)-\d+").unwrap();
+        }
+
+        let timestamp = match KEY_REGEX.captures(key) {
+            None => panic!("invalid event key passed to function"),
+            Some(captures) => captures["timestamp"].to_string()
+        };
+
+        timestamp.parse::<u64>().unwrap()
+    }
+
+    fn setup_connection(&self) -> InterfaceResult<redis::Connection> {
+        let connection = self.redis_client.get_connection();
+
+        match connection {
+            Err(error) => Err(InterfaceError::ConnectionError(error.to_string())),
+            Ok(connection) => Ok(connection)
+        }
+    }
+
+    fn get_service_event_by_key(&self, connection: &mut redis::Connection, event_key: &str) -> InterfaceResult<ServiceEvent> {
+        let event_data_list: Vec<HashMap<String, HashMap<String, String>>> = match connection.xrange_count(
+            &self.stream_name,
+            &event_key,
+            &event_key,
+            1
+        ) {
+            Err(error) => return Err(InterfaceError::DequeueError(error.to_string())),
+            Ok(data) => data
+        };
+
+        let event_data = match event_data_list.into_iter().next() {
+            None => return Err(InterfaceError::DequeueError(String::from("unexpected empty value in stream"))),
+            Some(event_data) => event_data
+        };
+
+        let event = match event_data.get(event_key) {
+            None => return Err(InterfaceError::DequeueError(String::from("expected event map, found None"))),
+            Some(event) => match event.get("event") {
+                None => return Err(InterfaceError::DequeueError(String::from("expected event at key \"event\", found None"))),
+                Some(event) => event
+            }
+        };
+
+        let event: ServiceEvent = match serde_json::from_str(&event) {
+            Err(error) => return Err(InterfaceError::JSONParseError(error.to_string())),
+            Ok(event) => event
+        };
+
+        Ok(event)
+    }
+
+    pub fn enqueue(&mut self, event: &ServiceEvent) -> InterfaceResult<()> {
+        let mut connection = self.setup_connection()?;
+
+        let event_as_json = match serde_json::to_string(&event) {
+            Err(error) => return Err(InterfaceError::JSONDumpError(error.to_string())),
+            Ok(json) => json
+        };
+
+        let event_key: String = match connection.xadd(
+            &self.stream_name,
+            "*",
+            &[("event", &event_as_json)]
+        ) {
+            Err(error) => return Err(InterfaceError::EnqueueError(error.to_string())),
+            Ok(key) => key
+        };
+
+        if let Err(error) = connection.lpush::<_, _, ()>(
+            &self.queue_name,
+            &event_key
+        ) {
+            return Err(InterfaceError::EnqueueError(error.to_string()));
+        }
+
         Ok(())
     }
 
-    pub fn dequeue(&mut self) -> InterfaceResult<'a, service_event::ServiceEvent<'a>> {
-        Err(InterfaceError::EmptyQueue)
+    pub fn dequeue(&mut self) -> InterfaceResult<TimestampedEvent> {
+        let mut connection = self.setup_connection()?;
+
+        let event_key: String = match connection.rpop(&self.queue_name, None) {
+            Err(error) => return Err(InterfaceError::DequeueError(error.to_string())),
+            Ok(key) => match key {
+                None => return Err(InterfaceError::EmptyQueue),
+                Some(key) => key
+            }
+        };
+
+        let event = self.get_service_event_by_key(&mut connection, &event_key)?;
+        let timestamp = Self::extract_timestamp_from_event_key(&event_key);
+
+        Ok(TimestampedEvent(timestamp, event))
     }
 
-    pub fn await_response(&mut self, event: &service_event::ServiceEvent) -> InterfaceResult<'a, service_event::ServiceEvent<'a>> {
-        Err(InterfaceError::TimeoutExpired)
+    pub fn dequeue_blocking(&mut self, timeout: u16) -> InterfaceResult<TimestampedEvent> {
+        let mut connection = self.setup_connection()?;
+
+        let event_kvp: (String, String) = match connection.brpop(
+            &self.queue_name, 
+            timeout.into()
+        ) {
+            Err(error) => return Err(InterfaceError::DequeueError(error.to_string())),
+            Ok(key) => match key {
+                None => return Err(InterfaceError::EmptyQueue),
+                Some(kvp) => kvp
+            }
+        };
+
+        let event_key = event_kvp.1.clone();
+
+        let event = self.get_service_event_by_key(&mut connection, &event_key)?;
+        let timestamp = Self::extract_timestamp_from_event_key(&event_key);
+
+        Ok(TimestampedEvent(timestamp, event))
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+    use std::thread;
     use super::*;
 
     #[test]
     fn create_ok() {
         let interface = ServiceInterface::new(
-            "test_queue"
+            "test_queue",
+            "redis://127.0.0.1"
         );
 
         assert_eq!(interface.queue_name, "test_queue");
@@ -48,11 +191,12 @@ mod tests {
     #[test]
     fn enqueue_dequeue_ok() {
         let mut interface = ServiceInterface::new(
-            "test_event_enqueue_dequeue"
+            "test_event_enqueue_dequeue",
+            "redis://127.0.0.1"
         );
 
-        let event = service_event::ServiceEvent::new(
-            0,
+        let event = ServiceEvent::new(
+            10,
             "test_enqueue",
             None
         );
@@ -61,41 +205,40 @@ mod tests {
 
         let result = interface.dequeue().unwrap();
 
-        assert_eq!(event, result);
+        assert_eq!(&event, result.get_event());
     }
 
     #[test]
-    fn await_blocking_timeout() {
+    fn dequeue_blocking_ok() {
         let mut interface = ServiceInterface::new(
-            "test_event_enqueue_dequeue"
+            "test_event_dequeue_blocking",
+            "redis://127.0.0.1"
         );
 
-        let event = service_event::ServiceEvent::new(
+        let event = ServiceEvent::new(
             10,
             "test_enqueue",
-            None
+            Some(String::from("Payload!"))
         );
 
-        let result = interface.await_response(&event);
+        let event_uuid = event.get_uuid();
 
-        assert_eq!(result, Err(InterfaceError::TimeoutExpired));
-    }
+        let handle = thread::spawn(move || {
+            thread::sleep(Duration::from_millis(1000));
 
-    #[test]
-    fn await_blocking_ok() {
-        let mut interface = ServiceInterface::new(
-            "test_event_enqueue_dequeue"
-        );
+            let mut local_interface = ServiceInterface::new(
+                "test_event_dequeue_blocking",
+                "redis://127.0.0.1"
+            );
 
-        let event = service_event::ServiceEvent::new(
-            10,
-            "test_enqueue",
-            Some("Ping!")
-        );
+            local_interface.enqueue(&event).unwrap();
+        });
 
-        let result = interface.await_response(&event).unwrap();
+        let result = interface.dequeue_blocking(10).unwrap();
 
-        assert_eq!(event.get_uuid(), result.get_uuid());
-        assert_eq!(result.get_payload(), Some("Pong!"));
+        handle.join().unwrap();
+
+        assert_eq!(event_uuid, result.get_event().get_uuid());
+        assert_eq!(result.get_event().get_payload(), Some(String::from("Payload!")));
     }
 }
--- src/service_interface/service_event.rs
+++ src/service_interface/service_event.rs
@@ -1,61 +1,81 @@
 use uuid::Uuid;
+use serde::{Serialize, Deserialize};
 
-#[derive(Debug, PartialEq)]
-pub struct ServiceEvent<'a> {
+/// A ServiceEvent contains information that is passed to other services by the communication backbone
+/// 
+/// - The [`request_uuid`] can be assumed to be unique between services, but collissions may happen, although this chance is very low
+/// - The [`timeout`] is specified in seconds since queueing the request
+/// - The [`action`] is an arbitrary string
+/// - The [`payload`] is serialized data in an agreed upon format (commonly JSON)
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct ServiceEvent {
     request_uuid: u128,
-    timestamp: Option<u64>,
     timeout: u16,
-    action: &'a str,
-    payload: Option<&'a str>
+    action: String,
+    payload: Option<String>
 }
 
-impl<'a> ServiceEvent<'a> {
-    pub fn new(timeout: u16, action: &'a str, payload: Option<&'a str>) -> Self {
+impl ServiceEvent {
+    /// Create a service event
+    /// 
+    /// A new event is created with a timeout, payload, and action.
+    /// On creating an event, a new uuid is generated and assigned. When creating a response from an event, the old event's uuid is reused to identify the response
+    /// - `timeout` must be non-zero, timeout is specified in seconds
+    /// - `action` is an arbitrary string meaningful to consumers
+    /// - `payload` is serialized data in a common format such as JSON. This format may differ between services, but should be decided upon when designing them.
+    /// 
+    /// Example:
+    /// ```
+    /// use common_service_interface::service_interface::ServiceEvent;
+    /// 
+    /// let event = ServiceEvent::new(10, "my_event", Some("{ \"foo\": \"bar\" }".to_string()));
+    /// ```
+    /// 
+    pub fn new(timeout: u16, action: &str, payload: Option<String>) -> Self {
         let request_uuid = Uuid::new_v4();
         let request_uuid = request_uuid.as_u128();
-        let timestamp = None;
+
+        if timeout == 0 {
+            panic!("timeout may not be zero")
+        }
 
         ServiceEvent {
             request_uuid,
-            timestamp,
             timeout,
-            action,
+            action: String::from(action),
             payload
         }
     }
 
-    pub fn set_uuid(&mut self, request_uuid: u128) {
-        self.request_uuid = request_uuid
+    /// Create a service event as response on another response
+    /// 
+    /// A response reuses the event uuid to identify it. Other than reusing a uuid, this functions acts the same as `ServiceEvent::new()`
+    ///  
+    pub fn new_response(event: &ServiceEvent, timeout: u16, action: &str, payload: Option<String>) -> Self {
+        let mut new_event = ServiceEvent::new(timeout, action, payload);
+        new_event.request_uuid = event.request_uuid;
+
+        new_event
     }
 
     pub fn get_uuid(&self) -> u128 {
         self.request_uuid
     }
 
-    pub fn set_timestamp(&mut self, timestamp: u64) -> Result<(), &str> {
-        if let Some(_) = self.timestamp {
-            return Err("can set event timestamp only once")
-        }
-
-        self.timestamp = Some(timestamp);
-
-        Ok(())
-    }
-
-    pub fn get_timestamp(&self) -> Option<u64> {
-        self.timestamp
-    }
-
     pub fn get_timeout(&self) -> u16 {
         self.timeout
     }
 
-    pub fn get_action(&self) -> &'a str {
-        self.action
+    pub fn get_action(&self) -> &str {
+        &self.action
     }
 
-    pub fn get_payload(&self) -> Option<&'a str> {
-        self.payload
+    pub fn get_payload(&self) -> Option<String> {
+        match &self.payload {
+            None => None,
+            Some(str) => Some(str.to_string())
+        }
     }
 }
 
@@ -74,45 +94,23 @@ mod tests {
         assert_eq!(event.get_action(), "test_event_create");
         assert_eq!(event.get_payload(), None);
         assert_eq!(event.get_timeout(), 10);
-        assert_eq!(event.get_timestamp(), None);
     }
-
+    
     #[test]
-    fn set_timestamp() {
-        let mut event = ServiceEvent::new(
+    fn create_response_ok() {
+        let event_a = ServiceEvent::new(
             10,
             "test_event_create",
             None
         );
 
-        event.set_timestamp(42).unwrap();
-
-        assert_eq!(event.get_timestamp().unwrap(), 42);
-    }
-
-    #[test]
-    #[should_panic(expected="can set event timestamp only once")]
-    fn set_timestamp_twice() {
-        let mut event = ServiceEvent::new(
+        let event_b = ServiceEvent::new_response(
+            &event_a,
             10,
-            "test_event_create",
+            "test_event_response",
             None
         );
 
-        event.set_timestamp(42).unwrap();
-        event.set_timestamp(43).unwrap();
-    }
-
-    #[test]
-    fn change_uuid() {
-        let mut event = ServiceEvent::new(
-            10,
-            "test_event_create",
-            None
-        );
-
-        event.set_uuid(42);
-
-        assert_eq!(event.get_uuid(), 42);
+        assert_eq!(event_a.get_uuid(), event_b.get_uuid());
     }
 }
